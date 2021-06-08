10f21375b402e86b026e6a3955fcb8b10cc82fb4
Camsera <cameronmetzinger@gmail.com>
2021-06-08 15:06:42 -0400

    added slider functionality and random color to divs

--- sketch.js
+++ sketch.js
@@ -1,16 +1,36 @@
 const divCreation = document.querySelector(".sketch-container");
-let gridNumber = Number(prompt("Enter a number for an *x* grid size"));
+let initialGridSize = 16;
+let gridNumber = document.querySelector("#slide");
+gridNumber.value = 16;
+
+
 
 function createGrid(gridNumber){
     for(let i = 0; i < gridNumber*gridNumber; i++){
-        const gridElement = document.createElement("div");
+        let gridElement = document.createElement("div");
         gridElement.classList = "grid-element";
-        //gridElement.addEventListener("mouseover", changeColor);
         divCreation.appendChild(gridElement);
     }
+    divCreation.setAttribute('style', `grid-template-columns: repeat(${gridNumber}, 1fr); grid-template-rows: repeat(${gridNumber}, 1fr);`);
+    let gridContainer = divCreation.querySelectorAll('div');
+    gridContainer.forEach(gridElement => gridElement.addEventListener('mouseover', colorGrid));
 }
-function setGridSize(gridNumber){
-    divCreation.setAttribute('style', `grid-template-columns: repeat(${gridNumber}, 2fr); grid-template-rows: repeat(${gridNumber}, 2fr);`)
+function recreateGrid(){
+    let gridContainer = divCreation.querySelectorAll('div');
+    gridContainer.forEach(gridElement => gridElement.remove());
+    createGrid(gridNumber.value);
+}
+
+function colorGrid(e){
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    let currentColor = [red, green, blue];
+    e.target.style = `background-color: rgb(${currentColor})`;
+}
+ function outputUpdate(boxAmount){
+     document.querySelector('#box-amount').value = boxAmount;
  }
-createGrid(gridNumber);
-setGridSize(gridNumber);
+ createGrid(initialGridSize);
+ gridNumber.addEventListener("mouseup", recreateGrid)
+ 
